9f195d2e3423f95574ed7e827c4a7ab56c9955ab
Jeremy Miranda <Jeremy.D.Miranda@gmail.com>
2015-12-09 22:07:55 -0600

    this renders the map

--- scripts/site.js
+++ scripts/site.js
@@ -109,34 +109,28 @@ var json = $.getJSON('https://s3.amazonaws.com/journeyfranklin/groups.json', fun
 // var map = L.mapbox.map('map', 'mapbox.pencil')
 //     .setView([35.9292, -86.8575], 9);
 
-
-L.mapbox.accessToken = 'pk.eyJ1IjoiYW1icmlhc2hpciIsImEiOiJjaWZ0MXAybDcwZ3I2dHRseWI3NjAyMTZ2In0.eD7uxIRAY9ifI6ecnkiu-g';
-var map = L.mapbox.map('map', 'mapbox.pencil')
-    .setView([35.9292, -86.8575], 9);
-
-
-var featureLayer = L.mapbox.featureLayer({
-        type: 'FeatureCollection',
-        features: [{
-            type: 'Feature',
-            properties: {
-                'marker-color': '#548cba',
-                'marker-size': 'large',
-                'marker-symbol': 'religious-christian'
-            },
-            geometry: {
-                type: 'Point',
-                coordinates: [ -86.8594, 35.9318]
-            }
-        }]
-    })
-  .bindPopup('data about group here')
-    .addTo(map);
+// var featureLayer = L.mapbox.featureLayer({
+//         type: 'FeatureCollection',
+//         features: [{
+//             type: 'Feature',
+//             properties: {
+//                 'marker-color': '#548cba',
+//                 'marker-size': 'large',
+//                 'marker-symbol': 'religious-christian'
+//             },
+//             geometry: {
+//                 type: 'Point',
+//                 coordinates: [ -86.8594, 35.9318]
+//             }
+//         }]
+//     })
+//   .bindPopup('data about group here')
+//     .addTo(map);
 
 // The HTML we put in bindPopup doesn't exist yet, so we can't just say
 // $('#mybutton'). Instead, we listen for click events on the map element which
 // will bubble up from the tooltip, once it's created and someone clicks on it.
-$('#map').on('click', '.trigger', function() {
-    alert('something goes here');
-});
+// $('#map').on('click', '.trigger', function() {
+//     alert('something goes here');
+// });
 

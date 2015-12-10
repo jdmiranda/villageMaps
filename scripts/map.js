650f1b645778949289586c4ca288966d1c4c67a8
Jeremy Miranda <Jeremy.D.Miranda@gmail.com>
2015-12-09 22:11:52 -0600

    dumb dumb dumb

---
+++ scripts/map.js
@@ -0,0 +1,21 @@
+L.mapbox.accessToken = 'pk.eyJ1IjoiYW1icmlhc2hpciIsImEiOiJjaWZ0MXAybDcwZ3I2dHRseWI3NjAyMTZ2In0.eD7uxIRAY9ifI6ecnkiu-g';
+var map = L.mapbox.map('map', 'mapbox.pencil')
+    .setView([35.9292, -86.8575], 9);
+    
+    var featureLayer = L.mapbox.featureLayer({
+        type: 'FeatureCollection',
+        features: [{
+            type: 'Feature',
+            properties: {
+                'marker-color': '#548cba',
+                'marker-size': 'large',
+                'marker-symbol': 'religious-christian'
+            },
+            geometry: {
+                type: 'Point',
+                coordinates: [ -86.8594, 35.9318]
+            }
+        }]
+    })
+  .bindPopup('data about group here')
+    .addTo(map);
